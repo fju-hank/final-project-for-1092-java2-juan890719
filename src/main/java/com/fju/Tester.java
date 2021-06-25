1b2f400f5058dab279d2a36afcab8b06074319c6
juan <michelle.70623@gmail.com>
2021-06-25 19:13:52 +0800

    add readLine json

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -2,6 +2,8 @@ package com.fju;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Json json = new Json();
+        String value = json.taipeiActivity();
+        System.out.println("result = " + value);
     }
 }
