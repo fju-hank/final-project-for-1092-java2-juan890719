4f6b42e29b65d81dbcebc92946a1705890834c33
juan <michelle.70623@gmail.com>
2021-06-27 05:43:32 +0800

    Add GUI v1...

--- src/main/java/com/fju/GUI.java
+++ src/main/java/com/fju/GUI.java
@@ -1,4 +1,20 @@
 package com.fju;
 
+import javax.swing.*;
+
 public class GUI {
+    private JPanel mainJPanel;
+    private JTextField name;
+    private JTextField type;
+    private JTextField price;
+    private JLabel meal;
+    private JButton addBtn;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setContentPane(new GUI().mainJPanel);
+        jFrame.setSize(600, 400);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
 }
